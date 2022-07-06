c30596d3e6e96f2cc71ed5ababd2e4934ebb8b84
mam-mih-val <mam.mih.val@gmail.com>
2022-07-06 18:53:06 +0300

    full chain

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -27,24 +27,24 @@ echo "/mnt/pool/nica/7/mam2mih/soft/basov/mcpico_to_analysis_tree/build/convert 
                                                         --boost-to-lab \
                                                         --$data_format
 
-#find . -name "output*.root" > list.txt
-#echo
-#date
-#echo "Running correction ..."
-#/mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-#                                                                          -i list.txt \
-#                                                                          -t atree \
-#                                                                          -o correction_out.root \
-#                                                                          --yaml-config-file /mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/setups/bmatn/mc_corrections.yml \
-#                                                                          --yaml-config-name BMN
-#echo
-#date
-#echo "Running correlation ..."
-#/mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
-#                                                                              --input-file correction_out.root \
-#                                                                              --configuration-file /mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/setups/bmatn/mc_correlations.yml \
-#                                                                              --configuration-name _tasks \
-#                                                                              -o correlation_out.root
-#
+find . -name "output*.root" > list.txt
+echo
+date
+echo "Running correction ..."
+/mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+                                                                          -i list.txt \
+                                                                          -t atree \
+                                                                          -o correction_out.root \
+                                                                          --yaml-config-file /mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/setups/bmatn/mc_corrections.yml \
+                                                                          --yaml-config-name BMN
+echo
+date
+echo "Running correlation ..."
+/mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
+                                                                              --input-file correction_out.root \
+                                                                              --configuration-file /mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/setups/bmatn/mc_correlations.yml \
+                                                                              --configuration-name _tasks \
+                                                                              -o correlation_out.root
+
 
 echo PROCESS FINISHED
