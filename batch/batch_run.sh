5228f18253033d253dd0db636c0ad30c833dc56b
mam-mih-val <mam.mih.val@gmail.com>
2022-07-10 18:22:19 +0300

    Checking the QnAnalysis

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -35,20 +35,26 @@ find . -name "output*.root" > list.txt
 echo
 date
 echo "Running correction ..."
-/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-                                                                          -i list.txt \
-                                                                          -t atree \
-                                                                          -o correction_out.root \
-                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/mc_corrections.yml \
-                                                                          --yaml-config-name BMN
-echo
-date
-echo "Running correlation ..."
-/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
-                                                                              --input-file correction_out.root \
-                                                                              --configuration-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/mc_correlations.yml \
-                                                                              --configuration-name _tasks \
-                                                                              -o correlation_out.root
+
+/scratch1/mmamaev/mcpico_to_analysis_tree/build/convert \
+                                                        -i list.txt \
+                                                        -o out.root \
+                                                        -N -1
+
+#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+#                                                                          -i list.txt \
+#                                                                          -t atree \
+#                                                                          -o correction_out.root \
+#                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/mc_corrections.yml \
+#                                                                          --yaml-config-name BMN
+#echo
+#date
+#echo "Running correlation ..."
+#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
+#                                                                              --input-file correction_out.root \
+#                                                                              --configuration-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/mc_correlations.yml \
+#                                                                              --configuration-name _tasks \
+#                                                                              -o correlation_out.root
 
 
 echo PROCESS FINISHED
