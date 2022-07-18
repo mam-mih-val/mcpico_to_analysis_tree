60a8afc76225f84a5c398a648d9bba0c14112275
mam-mih-val <mam.mih.val@gmail.com>
2022-07-18 15:28:47 +0300

    - Without boost to lab system - Changing the path to setups

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -11,38 +11,41 @@ cd $output_dir
 mkdir -p $job_num
 cd $job_num
 
-#source /mnt/pool/nica/7/mam2mih/soft/basov/fairsoft/install/bin/thisroot.sh
-#echo
-#date
-#echo "Running conversion ..."
-#echo "/mnt/pool/nica/7/mam2mih/soft/basov/mcpico_to_analysis_tree/build/convert -i $filelist -o output.root --output-tree atree -s $colliding_system -e $collision_energy --sample-reaction-plane --boost-to-lab --$data_format"
-#
-#/mnt/pool/nica/7/mam2mih/soft/basov/mcpico_to_analysis_tree/build/convert \
-#                                                        -i $filelist \
-#                                                        -o output.root \
-#                                                        --output-tree atree \
-#                                                        -s $colliding_system \
-#                                                        -e $collision_energy \
-#                                                        --sample-reaction-plane \
-#                                                        --boost-to-lab \
-#                                                        --$data_format
-#
-#find . -name "output*.root" > list.txt
-#echo
-#date
-#echo "Running correction ..."
-#/mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-#                                                                          -i list.txt \
-#                                                                          -t atree \
-#                                                                          -o correction_out.root \
-#                                                                          --yaml-config-file /mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/setups/bmatn/mc_corrections.yml \
-#                                                                          --yaml-config-name BMN
+source /mnt/pool/nica/7/mam2mih/soft/basov/fairsoft/install/bin/thisroot.sh
+echo
+date
+echo "Running conversion ..."
+echo "/mnt/pool/nica/7/mam2mih/soft/basov/mcpico_to_analysis_tree/build/convert -i $filelist -o output.root --output-tree atree -s $colliding_system -e $collision_energy --sample-reaction-plane --$data_format"
+
+/mnt/pool/nica/7/mam2mih/soft/basov/mcpico_to_analysis_tree/build/convert \
+                                                        -i $filelist \
+                                                        -o output.root \
+                                                        --output-tree atree \
+                                                        -s $colliding_system \
+                                                        -e $collision_energy \
+                                                        --sample-reaction-plane \
+                                                        --$data_format
+
+find . -name "output*.root" > list.txt
+input_list=\"list.txt\"
+qa_output=\"qa.root\"
+root -q "/scratch1/mmamaev/bmnroot/analysis/common/macro/run_tracking_qa.C( $input_list, $qa_output )"
+
+echo
+date
+echo "Running correction ..."
+/mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+                                                                          -i list.txt \
+                                                                          -t atree \
+                                                                          -o correction_out.root \
+                                                                          --yaml-config-file /mnt/pool/nica/7/mam2mih/soft/basov/mcpico_to_analysis_tree/qnanalysis/mc_corrections.yml \
+                                                                          --yaml-config-name BMN
 echo
 date
 echo "Running correlation ..."
 /mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
                                                                               --input-file correction_out.root \
-                                                                              --configuration-file /mnt/pool/nica/7/mam2mih/soft/basov/QnAnalysis/setups/bmatn/mc_correlations.yml \
+                                                                              --configuration-file /mnt/pool/nica/7/mam2mih/soft/basov/mcpico_to_analysis_tree/qnanalysis/mc_correlations.yml \
                                                                               --configuration-name _tasks \
                                                                               -o correlation_out.root
 
