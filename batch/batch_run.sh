2215b67795842f309747ecbcf5427b7bf56e8709
mam-mih-val <mam.mih.val@gmail.com>
2022-10-17 15:28:42 +0300

    One more node is added to the exception list

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -l h=!(ncx152.jinr.ru|ncx205.jinr.ru|ncx123.jinr.ru|ncx111.jinr.ru|ncx113.jinr.ru|ncx149.jinr.ru|ncx223.jinr.ru)
+#$ -l h=!(ncx152.jinr.ru|ncx205.jinr.ru|ncx123.jinr.ru|ncx111.jinr.ru|ncx113.jinr.ru|ncx149.jinr.ru|ncx223.jinr.ru|ncx231.jinr.ru)
 
 format='+%Y/%m/%d-%H:%M:%S'
 
