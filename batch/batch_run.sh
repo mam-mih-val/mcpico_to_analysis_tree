1e6391c59123ed653880b2c24b4ba984b4e7f410
mam-mih-val <mam.mih.val@gmail.com>
2022-10-17 14:34:34 +0300

    Boost to lab system

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -19,7 +19,7 @@ source /scratch1/mmamaev/bmn_environment.sh
 echo
 date
 echo "Running conversion ..."
-echo "/scratch1/mmamaev/mcpico_to_analysis_tree/build/convert -i $filelist -o output.root --output-tree atree -s $colliding_system -e $collision_energy --sample-reaction-plane --$data_format"
+echo "/scratch1/mmamaev/mcpico_to_analysis_tree/build/convert -i $filelist -o output.root --output-tree atree -s $colliding_system -e $collision_energy --sample-reaction-plane --boost-to-lab --$data_format"
 
 /scratch1/mmamaev/mcpico_to_analysis_tree/build/convert \
                                                         -i $filelist \
@@ -28,6 +28,7 @@ echo "/scratch1/mmamaev/mcpico_to_analysis_tree/build/convert -i $filelist -o ou
                                                         -s $colliding_system \
                                                         -e $collision_energy \
                                                         --sample-reaction-plane \
+                                                        --boost-to-lab \
                                                         --$data_format
 
 find . -name "output*.root" > list.txt
