a5ead7463955db47b67cf15ab2fa812c38443ae9
Mikhail <mam.mih.val@gmail.com>
2022-12-05 19:13:15 +0300

    Correction and correlation steps are performed in a job Boost to lab system is switched off

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -20,7 +20,7 @@ source /scratch1/mmamaev/bmn_environment.sh
 echo
 date
 echo "Running conversion ..."
-echo "/scratch1/mmamaev/mcpico_to_analysis_tree/build/convert -i $filelist -o output.root --output-tree atree -s $colliding_system -e $collision_energy --sample-reaction-plane --boost-to-lab --$data_format"
+echo "/scratch1/mmamaev/mcpico_to_analysis_tree/build/convert -i $filelist -o output.root --output-tree atree -s $colliding_system -e $collision_energy --sample-reaction-plane --$data_format"
 
 /scratch1/mmamaev/mcpico_to_analysis_tree/build/convert \
                                                         -i $filelist \
@@ -29,8 +29,8 @@ echo "/scratch1/mmamaev/mcpico_to_analysis_tree/build/convert -i $filelist -o ou
                                                         -s $colliding_system \
                                                         -e $collision_energy \
                                                         --sample-reaction-plane \
-                                                        --boost-to-lab \
                                                         --$data_format
+#                                                        --boost-to-lab \
 
 find . -name "output*.root" > list.txt
 echo; date
@@ -39,20 +39,20 @@ input_list=\"list.txt\"
 output_qa=\"qa.root\"
 root -q "/scratch1/mmamaev/mcpico_to_analysis_tree/macro/run_qa.cc($input_list, $output_qa)"
 echo; date
-#echo "Running correction ..."
-#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-#                                                                          -i list.txt \
-#                                                                          -t atree \
-#                                                                          -o correction_out.root \
-#                                                                          --yaml-config-file /scratch1/mmamaev/mcpico_to_analysis_tree/qnanalysis/mc_corrections.yml \
-#                                                                          --yaml-config-name BMN
-#echo; date
-#echo "Running correlation ..."
-#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
-#                                                                              --input-file correction_out.root \
-#                                                                              --configuration-file /scratch1/mmamaev/mcpico_to_analysis_tree/qnanalysis/mc_correlations.yml \
-#                                                                              --configuration-name _tasks \
-#                                                                              -o correlation_out.root
+echo "Running correction ..."
+/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+                                                                          -i list.txt \
+                                                                          -t atree \
+                                                                          -o correction_out.root \
+                                                                          --yaml-config-file /scratch1/mmamaev/mcpico_to_analysis_tree/qnanalysis/mc_corrections.yml \
+                                                                          --yaml-config-name BMN
+echo; date
+echo "Running correlation ..."
+/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
+                                                                              --input-file correction_out.root \
+                                                                              --configuration-file /scratch1/mmamaev/mcpico_to_analysis_tree/qnanalysis/mc_correlations.yml \
+                                                                              --configuration-name _tasks \
+                                                                              -o correlation_out.root
 
 rm output*.root list.txt correction_out.root
 
