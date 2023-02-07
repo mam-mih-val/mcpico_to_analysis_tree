b081a4542b9c12e6065e8bf06a577ddc7c967151
Mikhail <mam.mih.val@gmail.com>
2023-02-07 13:24:31 +0300

    Another correction and correlation setups to study the momentum conservation effects

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -44,13 +44,13 @@ echo "Running correction ..."
                                                                           -i list.txt \
                                                                           -t atree \
                                                                           -o correction_out.root \
-                                                                          --yaml-config-file /scratch1/mmamaev/mcpico_to_analysis_tree/qnanalysis/mc_corrections.yml \
+                                                                          --yaml-config-file /scratch1/mmamaev/mcpico_to_analysis_tree/qnanalysis/pT_conservation_corrections.yml \
                                                                           --yaml-config-name BMN
 echo; date
 echo "Running correlation ..."
 /scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
                                                                               --input-file correction_out.root \
-                                                                              --configuration-file /scratch1/mmamaev/mcpico_to_analysis_tree/qnanalysis/mc_correlations.yml \
+                                                                              --configuration-file /scratch1/mmamaev/mcpico_to_analysis_tree/qnanalysis/pT_consetvation_correlations.yml \
                                                                               --configuration-name _tasks \
                                                                               -o correlation_out.root
 
